675ec547fc6d8823b15df0e7c7a787f3a195cda4
ViktorF <logforpractik@yandex.ru>
2022-01-07 22:12:33 +0300

    реализовано сохранение информации о пользователе

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,13 +6,24 @@ const profileName = profile.querySelector('.profile__name')
 const profileDescription = profile.querySelector('.profile__description')
 const inputName = popup.querySelector('.profile-edit-form__input_value_name')
 const inputDescription = popup.querySelector('.profile-edit-form__input_value_description')
+const profileEditForm = popup.querySelector('.profile-edit-form')
 
+// показывает модальное окно редактирования профиля
 editBtn.addEventListener('click', function() {
   popup.classList.add('popup_opened')
   inputName.value = profileName.textContent
   inputDescription.value = profileDescription.textContent
 })
 
+// закрывает модальное окно
 closeBtn.addEventListener('click', function() {
+  popup.classList.remove('popup_opened')
+})
+
+// сохранение информации в профиле
+profileEditForm.addEventListener('submit', function(e) {
+  e.preventDefault()
+  profileName.textContent = inputName.value
+  profileDescription.textContent = inputDescription.value
   popup.classList.remove('popup_opened')
 })
